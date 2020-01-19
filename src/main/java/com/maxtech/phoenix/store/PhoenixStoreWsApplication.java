dfb084a94d2cda2806a9649bba6014ceb2f4d8aa
MaxmdLira <maxwell.lira@poggers.com.br>
2020-01-18 23:16:06 -0300

    add mock of categories

--- src/main/java/com/maxtech/phoenix/store/PhoenixStoreWsApplication.java
+++ src/main/java/com/maxtech/phoenix/store/PhoenixStoreWsApplication.java
@@ -1,13 +1,27 @@
 package com.maxtech.phoenix.store;
 
+import com.maxtech.phoenix.store.domain.Categoria;
+import com.maxtech.phoenix.store.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class PhoenixStoreWsApplication {
+public class PhoenixStoreWsApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhoenixStoreWsApplication.class, args);
 	}
+	@Autowired
+	CategoriaRepository repository;
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		repository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
